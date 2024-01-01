43635c62ab97efb24828e6a7924424d5d4f1da22
Pinaki <appugupta007@gmail.com>
2024-01-01 12:57:14 +0530

    NEW: Desktop entries and app icons.

---
+++ PortablePrograms/llama.cpp/MiniChat-1.5-3B-GGUF.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# https://github.com/garyexplains/examples/blob/master/how-to-run-llama-cpp-on-raspberry-pi.md
+# https://huggingface.co/TheBloke/Llama-2-7b-Chat-GGUF/tree/main
+# https://huggingface.co/TheBloke/CodeLlama-7B-Instruct-GGUF/tree/main
+
+# https://huggingface.co/tsunemoto/MiniChat-1.5-3B-GGUF
+# https://huggingface.co/tsunemoto/MiniChat-1.5-3B-GGUF/resolve/main/minichat-1.5-3b.Q2_K.gguf
+
+cd /mnt/hdd/HOME/llama.cpp \
+
+./main  -m models/minichat-1.5-3b.Q2_K.gguf --color \
+       --ctx_size 2048 \
+       -n -1 \
+       -ins -b 256 \
+       --top_k 10000 \
+       --temp 0.2 \
+       --repeat_penalty 1.1
